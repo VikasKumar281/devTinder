b1a4f5500081ee5a72e32ba4d984217ec7f88ca0
VikasKumar281 <vikaskumar280204@gmail.com>
2025-01-31 11:07:58 +0530

    multiple-route handlers

--- src/app.js
+++ src/app.js
@@ -1,45 +1,35 @@
 const express = require("express");
 
-
 const app = express();
 
-app.get("/user" , ( req , res) => {
-    console.log(req.query);
-    res.send({firstName:"Vikas" , secondName:"Kumar"});
-});
+app.get("/user" , ( req , res , next) => {
+    //the function is Known as Route Handler.
+    console.log("Handling the route user");
+    // next();
+    // res.send("Route Handler 1"); 
+    next();
+} ,
+[( req , res , next) => {
+    console.log("Handling the route user 2");
+    // res.send("Route Handler 2");
+    next();
+},
+( req , res , next) => {
+    console.log("Handling the route user 3");
+    // res.send("Route Handler 3");
+    next();
+}],
+( req , res , next) => {
+    console.log("Handling the route user 4");
+    // res.send("Route Handler 4");
+    next();
+},
+( req , res) => {
+    console.log("Handling the route user 5");
+    res.send("Route Handler 5");
+},
 
-//DYNAMIC ROUTES-->
-app.get("/user/:userId/:name/:password" , ( req , res) => {
-    console.log(req.params);
-    res.send({firstName:"Vikas" , secondName:"Kumar"});
-});
-
-
-app.get(/.*fly/ , ( req , res) => {
-    // regex --> /a/
-    //regex --> /.*fly/
-    res.send({firstName:"Vikas" , secondName:"Kumar"});
-});
-
-app.get("/ab?c" , ( req , res) => {
-    // b is optional
-    res.send({firstName:"Vikas" , secondName:"Kumar"});
-});
-
-app.get("/a(bc)?d" , ( req , res) => {
-    // bc is optional
-    res.send({firstName:"Vikas" , secondName:"Kumar"});
-});
-
-app.get("/ab+c" , ( req , res) => {
-    // a and c at the last and there can be you can add many b.
-    res.send({firstName:"Vikas" , secondName:"Kumar"});
-});
-
-app.get("/ab*cd" , ( req , res) => {
-    // anything you can insert between ab and cd , it will run.
-    res.send({firstName:"Vikas" , secondName:"Kumar"});
-});
+);
 
 app.listen(7777 , () => {
     console.log("Server is successfully listening on port 7777....");
