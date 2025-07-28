f10d665133d9771a70c0fa2223815b07123fc6f5
VikasKumar281 <vikaskumar280204@gmail.com>
2025-07-28 13:05:56 +0530

    new cors issue fixed

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ require("./utils/cronjob");
 
 
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:["http://localhost:5173","https://dev-tinder-web-gules.vercel.app/"
+],
    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
    credentials: true,
