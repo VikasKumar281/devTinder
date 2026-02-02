59bbe5602c0b4f5d7a992981413a90347b750ee1
VikasKumar281 <vikaskumar280204@gmail.com>
2026-02-02 23:28:35 +0530

    Some Errors fixed

--- src/models/user.js
+++ src/models/user.js
@@ -107,6 +107,115 @@
 
 
 
+// const mongoose = require("mongoose");
+// const validator = require("validator");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     firstName: {
+//       type: String,
+//       required: true,
+//       minLength: 4,
+//       maxLength: 50,
+//     },
+//     lastName: {
+//       type: String,
+//     },
+//     emailId: {
+//       type: String,
+//       lowercase: true,
+//       required: true,
+//       unique: true,
+//       trim: true,
+//       validate(value) {
+//         if (!validator.isEmail(value)) {
+//           throw new Error("Invalid email address: " + value);
+//         }
+//       },
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//       validate(value) {
+//         if (!validator.isStrongPassword(value)) {
+//           throw new Error("Enter a Strong Password: " + value);
+//         }
+//       },
+//     },
+//     age: {
+//       type: Number,
+//       min: 18,
+//     },
+//     gender: {
+//       type: String,
+//       enum: {
+//         values: ["male", "female", "other"],
+//         message: `{VALUE} is not a valid gender type`,
+//       },
+//       // validate(value) {
+//       //   if (!["male", "female", "others"].includes(value)) {
+//       //     throw new Error("Gender data is not valid");
+//       //   }
+//       // },
+//     },
+//     isPremium: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     membershipType: {
+//       type: String,
+//     },
+//     photoUrl: {
+//       type: String,
+//       maxlength:1000,
+//       trim: true,
+//       default: "https://geographyandyou.com/images/user-profile.png",
+//       validate(value) {
+//         if (!validator.isURL(value)) {
+//           throw new Error("Invalid Photo URL: " + value);
+//         }
+//       },
+//     },
+//     about: {
+//       type: String,
+//       maxlength:1000,
+//       trim: true,
+//       default: "This is a default about of the user!",
+//     },
+//     skills: {
+//       type: [String],
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// userSchema.methods.getJWT = async function () {
+//   const user = this;
+
+//   const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$356", {
+//     expiresIn: "7d",
+//   });
+
+//   return token;
+// };
+
+// userSchema.methods.validatePassword = async function (passwordInputByUser) {
+//   const user = this;
+//   const passwordHash = user.password;
+
+//   const isPasswordValid = await bcrypt.compare(
+//     passwordInputByUser,
+//     passwordHash
+//   );
+
+//   return isPasswordValid;
+// };
+
+// module.exports = mongoose.model("User", userSchema);
 
 
 
@@ -134,18 +243,13 @@ const userSchema = new mongoose.Schema(
       trim: true,
       validate(value) {
         if (!validator.isEmail(value)) {
-          throw new Error("Invalid email address: " + value);
+          throw new Error("Invalid email address");
         }
       },
     },
     password: {
       type: String,
       required: true,
-      validate(value) {
-        if (!validator.isStrongPassword(value)) {
-          throw new Error("Enter a Strong Password: " + value);
-        }
-      },
     },
     age: {
       type: Number,
@@ -153,37 +257,20 @@ const userSchema = new mongoose.Schema(
     },
     gender: {
       type: String,
-      enum: {
-        values: ["male", "female", "other"],
-        message: `{VALUE} is not a valid gender type`,
-      },
-      // validate(value) {
-      //   if (!["male", "female", "others"].includes(value)) {
-      //     throw new Error("Gender data is not valid");
-      //   }
-      // },
-    },
-    isPremium: {
-      type: Boolean,
-      default: false,
-    },
-    membershipType: {
-      type: String,
+      enum: ["male", "female", "other"],
     },
     photoUrl: {
       type: String,
-      maxlength:1000,
       trim: true,
       default: "https://geographyandyou.com/images/user-profile.png",
       validate(value) {
         if (!validator.isURL(value)) {
-          throw new Error("Invalid Photo URL: " + value);
+          throw new Error("Invalid photo URL");
         }
       },
     },
     about: {
       type: String,
-      maxlength:1000,
       trim: true,
       default: "This is a default about of the user!",
     },
@@ -191,31 +278,21 @@ const userSchema = new mongoose.Schema(
       type: [String],
     },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-userSchema.methods.getJWT = async function () {
-  const user = this;
-
-  const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$356", {
-    expiresIn: "7d",
-  });
-
-  return token;
+// JWT
+userSchema.methods.getJWT = function () {
+  return jwt.sign(
+    { _id: this._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
 };
 
-userSchema.methods.validatePassword = async function (passwordInputByUser) {
-  const user = this;
-  const passwordHash = user.password;
-
-  const isPasswordValid = await bcrypt.compare(
-    passwordInputByUser,
-    passwordHash
-  );
-
-  return isPasswordValid;
+// Password check
+userSchema.methods.validatePassword = function (passwordInput) {
+  return bcrypt.compare(passwordInput, this.password);
 };
 
 module.exports = mongoose.model("User", userSchema);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -124,42 +124,127 @@
 
 
 
+// const express = require("express");
+// const authRouter = express.Router();
+
+// const { validateSignUpData } = require("../utils/validation");
+// const User = require("../models/user");
+// const bcrypt = require("bcrypt");
+
+// authRouter.post("/signup", async (req, res) => {
+//   try {
+//     // Validation of data
+//     validateSignUpData(req);
+
+//     const { firstName, lastName, emailId, password } = req.body;
+
+//     // Encrypt the password
+//     const passwordHash = await bcrypt.hash(password, 10);
+//     console.log(passwordHash);
+
+//     //   Creating a new instance of the User model
+//     const user = new User({
+//       firstName,
+//       lastName,
+//       emailId,
+//       password: passwordHash,
+//     });
+
+//     const savedUser = await user.save();
+//     const token = await savedUser.getJWT();
+
+//     res.cookie("token", token, {
+//       expires: new Date(Date.now() + 8 * 3600000),
+//     });
+
+//     res.json({ message: "User Added successfully!", data: savedUser });
+//   } catch (err) {
+//     res.status(400).send("ERROR : " + err.message);
+//   }
+// });
+
+// authRouter.post("/login", async (req, res) => {
+//   try {
+//     const { emailId, password } = req.body;
+
+//     const user = await User.findOne({ emailId: emailId });
+//     if (!user) {
+//       throw new Error("Invalid credentials");
+//     }
+//     const isPasswordValid = await user.validatePassword(password);
+
+//     if (isPasswordValid) {
+//       const token = await user.getJWT();
+
+//       res.cookie("token", token, {
+//         expires: new Date(Date.now() + 8 * 3600000),
+//       });
+//       res.send(user);
+//     } else {
+//       throw new Error("Invalid credentials");
+//     }
+//   } catch (err) {
+//     res.status(400).send("ERROR : " + err.message);
+//   }
+// });
+
+// authRouter.post("/logout", async (req, res) => {
+//   res.cookie("token", null, {
+//     expires: new Date(Date.now()),
+//   });
+//   res.send("Loggged out Successfullly!!");
+// });
+
+// module.exports = authRouter;
+
+
+
 const express = require("express");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const User = require("../models/user");
+
 const authRouter = express.Router();
 
-const { validateSignUpData } = require("../utils/validation");
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const cookieOptions = {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+};
 
 authRouter.post("/signup", async (req, res) => {
   try {
-    // Validation of data
-    validateSignUpData(req);
-
     const { firstName, lastName, emailId, password } = req.body;
 
-    // Encrypt the password
-    const passwordHash = await bcrypt.hash(password, 10);
-    console.log(passwordHash);
+    if (!firstName || !emailId || !password) {
+      throw new Error("All required fields must be filled");
+    }
 
-    //   Creating a new instance of the User model
-    const user = new User({
+    if (!validator.isStrongPassword(password)) {
+      throw new Error("Password is not strong enough");
+    }
+
+    const existingUser = await User.findOne({ emailId });
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
       firstName,
       lastName,
       emailId,
       password: passwordHash,
     });
 
-    const savedUser = await user.save();
-    const token = await savedUser.getJWT();
+    const token = user.getJWT();
+    res.cookie("token", token, cookieOptions);
 
-    res.cookie("token", token, {
-      expires: new Date(Date.now() + 8 * 3600000),
-    });
-
-    res.json({ message: "User Added successfully!", data: savedUser });
+    res.status(201).json({ data: user });
   } catch (err) {
-    res.status(400).send("ERROR : " + err.message);
+    res.status(400).send(err.message);
   }
 });
 
@@ -167,32 +252,33 @@ authRouter.post("/login", async (req, res) => {
   try {
     const { emailId, password } = req.body;
 
-    const user = await User.findOne({ emailId: emailId });
+    const user = await User.findOne({ emailId });
     if (!user) {
       throw new Error("Invalid credentials");
     }
-    const isPasswordValid = await user.validatePassword(password);
 
-    if (isPasswordValid) {
-      const token = await user.getJWT();
-
-      res.cookie("token", token, {
-        expires: new Date(Date.now() + 8 * 3600000),
-      });
-      res.send(user);
-    } else {
+    const isValid = await user.validatePassword(password);
+    if (!isValid) {
       throw new Error("Invalid credentials");
     }
+
+    const token = user.getJWT();
+    res.cookie("token", token, cookieOptions);
+
+    res.json(user);
   } catch (err) {
-    res.status(400).send("ERROR : " + err.message);
+    res.status(400).send(err.message);
   }
 });
 
-authRouter.post("/logout", async (req, res) => {
+authRouter.post("/logout", (req, res) => {
   res.cookie("token", null, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
     expires: new Date(Date.now()),
   });
-  res.send("Logout Successful!!");
+  res.send("Logged out successfully");
 });
 
 module.exports = authRouter;
